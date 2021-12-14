5e760e4b3fa82eb1ab3cfe27a51abc81f30ee9d6
mamietian(田梦敏) <mamietian@tencent.com>
2021-12-14 15:43:14 +0800

    fix

--- worker/workplace/business.py
+++ worker/workplace/business.py
@@ -282,8 +282,7 @@ class MainHandler(BaseWorker):
     def ffmpeg_cmd(self):
         pass
 
-    def
-        (self):
+    def ffmpeg_cmds(self):
         pass
 
     def event_handler_loop(self):
@@ -463,7 +462,7 @@ class VodToRtmpHandler(MainHandler):
         self.ffmpeg_cmd_list = cmd.split(' ')
 
     def ffmpeg_cmds(self):
-        source_urls=self.task_struct.get('SourceUrls')[0],
+        source_urls=self.task_struct.get('SourceUrls'),
         stream_handler_duration = int(self.task_struct.get('StreamHandlerDuration'))
         stream_handler_start = int(self.task_struct.get('StreamHandlerStart'))
         loop = int(self.task_struct.get('Loop'))
